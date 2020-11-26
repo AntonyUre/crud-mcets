a9233d5c99428b4f8ed453fd3f3de635e4152121
antonyur <ureta.art@hotmail.com>
2020-11-26 00:00:00 -0500

    mi estructura

---
+++ src/components/ModalCreate.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="windows-modal">
+    <div class="modal-container close">
+      <p>
+        Añadir Usuario
+        <button class="close-modal">X</button>
+      </p>
+      <form action="" class="inputs">
+        <div>
+          <input class="medio" type="text" placeholder="Nombre" required/>
+          <input class="medio" type="text" placeholder="Apellido"/>
+        </div>
+        <div>
+          <input type="text" placeholder="Edad" />
+          <input type="number" placeholder="Dni" />
+          <select name="country">
+            <option value=" ">País</option>
+            <option>Perú</option>
+            <option>Argentina</option>
+            <option>México</option>
+            <option>Usa</option>
+          </select>
+        </div>
+        <div class="send">
+          <button type="button" class="btn">Aceptar</button>
+          <button type="button" class="btn">Cancelar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalCreate',
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+    <header>
+      <nav>
+        <a href="https://antonyur.com/" target="_blank">
+          <img src="@/assets/img/Logo-mcets.svg" alt="">
+        </a>
+        <div>
+          <button id="crear" class="btn open-modal" @click="open">Agregar Usuario</button>
+        </div>
+      </nav>
+    </header>
+
+    <div class="list-container">
+      <div class="text-container">
+        <h2>Lista de usuarios</h2>
+        <p><span>0 </span>Usuarios </p>
+      </div>
+
+      <div class="list-items">
+        <div class="user">
+          <p>Antony Ureta Acuña</p>
+          <p>24 años</p>
+          <p>75552715</p>
+          <p>Perú</p>
+          <div class="icons">
+            <i class="far fa-edit"></i>
+            <i class="far fa-trash-alt"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <ModalCreate v-if='open_modal'/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ModalCreate from '@/components/ModalCreate.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    ModalCreate,
+  },
+  data() {
+    return {
+    open_modal: false,
+    };
+  },
+  methods: {
+    open(){
+      this.open_modal=true;
+    }
   },
 };
 </script>
+<style lang="scss">
+
+</style>
